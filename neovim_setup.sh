299bb3a248a3563fd0d5174b291490c94ecced81
1cy50ng3b0b <66748817+mbn-code@users.noreply.github.com>
2024-05-14 11:47:54 +0200

    script update to make it more like IDE setup.

--- neovim_setup.sh
+++ neovim_setup.sh
@@ -1,157 +1,155 @@
 #!/bin/bash
 
-# Define log file path
-LOG_FILE="$HOME/neovim_setup.log"
+# Install Neovim using Homebrew
+brew install neovim
 
-# Function for logging
-log() {
-  echo "$1" >> "$LOG_FILE"
-}
-
-# Begin logging
-log "Starting Neovim setup script..."
-
-# Detect the operating system
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS
-  log "Detected macOS."
-  INSTALL_CMD="brew install"
-elif [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" ]]; then
-  # Linux (Assuming it's using APT, you can adapt for other package managers)
-  log "Detected Linux."
-  INSTALL_CMD="sudo apt-get install"
-else
-  log "Unsupported operating system: $OSTYPE"
-  exit 1
-fi
-
-# Install essential tools
-log "Installing Git..."
-$INSTALL_CMD git 2>> "$LOG_FILE"
-
-# Install Node.js for JavaScript development
-log "Installing Node.js..."
-$INSTALL_CMD nodejs 2>> "$LOG_FILE"
-
-# Install Python development tools
-log "Installing Python development tools..."
-$INSTALL_CMD python3 python3-pip 2>> "$LOG_FILE"
-
-# Install python-psutil (you can adapt this for other packages)
-log "Installing python-psutil..."
-$INSTALL_CMD python-psutil 2>> "$LOG_FILE"
-
-# Install Neovim
-log "Installing Neovim..."
-$INSTALL_CMD neovim 2>> "$LOG_FILE"
-
-# Create Neovim configuration directory if it doesn't exist
-log "Creating Neovim configuration directory..."
+# Create Neovim config directory if it doesn't exist
 mkdir -p ~/.config/nvim
 
-# Create a Neovim configuration file
-log "Creating Neovim configuration file..."
-cat <<EOF > ~/.config/nvim/init.vim
-" Neovim Configuration
-" Enable line numbers
-set number
-
-" Syntax Highlighting
-syntax enable
-set background=dark
-
-" Plugin Manager (vim-plug)
+# Create and edit the Neovim init.vim file
+cat > ~/.config/nvim/init.vim << 'EOF'
+" Use vim-plug to manage plugins
 if empty(glob('~/.config/nvim/autoload/plug.vim'))
-  silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs
-    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
 endif
 
 " Plugins
 call plug#begin('~/.config/nvim/plugged')
-
-" YouCompleteMe
-Plug 'ycm-core/YouCompleteMe', { 'do': './install.py --ts-completer --all --clangd' }
-
-" coc.nvim
-Plug 'neoclide/coc.nvim', {'branch': 'release'}
-
-" Lightline
-Plug 'itchyny/lightline.vim'
-
-" Gruvbox Color Scheme
-Plug 'morhetz/gruvbox'
-
-" Autopairs
-Plug 'jiangmiao/auto-pairs'
-
-" Additional language support (Example: Python)
-Plug 'python-mode/python-mode'
-Plug 'Vimjas/vim-python-pep8-indent'
-
-" File explorer (NERDTree)
-Plug 'preservim/nerdtree'
-
+Plug 'preservim/nerdtree'               " File explorer
+Plug 'vim-airline/vim-airline'          " Status bar
+Plug 'vim-airline/vim-airline-themes'   " Airline themes
+Plug 'tpope/vim-fugitive'                " Git integration
+Plug 'dense-analysis/ale'                " Linting and syntax checking
+Plug 'neoclide/coc.nvim', {'branch': 'release'} " Language server protocol (LSP) support
+Plug 'scrooloose/nerdcommenter'         " Easy commenting
+Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' } " Auto-completion
+Plug 'SirVer/ultisnips'                 " Snippet manager
+Plug 'honza/vim-snippets'               " Snippets collection
+Plug 'ludovicchabant/vim-gutentags'     " Automatic tags management
+Plug 'vim-scripts/Conque-Shell'         " Terminal emulator
 call plug#end()
 
-" Configure Gruvbox Color Scheme
-colorscheme gruvbox
+" Plugin settings
+" NERDTree
+map <C-n> :NERDTreeToggle<CR>
 
-" AutoPairs
-let g:AutoPairsFlyMode = 1
+" Airline
+let g:airline_theme='gruvbox'
 
-" Lightline
-set laststatus=2
-let g:lightline = {
-      \ 'active': {
-      \   'left': [ [ 'mode', 'paste' ],
-      \             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
-      \ },
-      \ 'component_function': {
-      \   'gitbranch': 'fugitive#head'
-      \ },
-      \ 'component_expand': {
-      \   'filename': 'lightline#buffer#filename',
-      \ },
-      \ 'component_type': {
-      \   'readonly': 'error',
-      \ }
-      \ }
+" ALE
+let g:ale_linters = {
+  \   'javascript': ['eslint'],
+  \}
+let g:ale_fixers = {
+  \   'javascript': ['eslint'],
+  \}
 
-" NERDTree configuration
-let g:NERDTreeWinSize = 25
-nmap <C-n> :NERDTreeToggle<CR>
+" COC
+" You may want to configure specific languages here, e.g., for Python:
+" autocmd FileType python setl lsp
 
-" Save and Quit
-map <leader>q :wqa<CR>
+" Enable mouse support
+set mouse=a
+
+" Enable syntax highlighting
+syntax on
+
+" Set tab width to 2 spaces
+set tabstop=2
+set shiftwidth=2
+set expandtab
+
+" Enable line numbers
+set number
+
+" Highlight current line
+set cursorline
+
+" Show matching parentheses
+set showmatch
+
+" Highlight search results
+set hlsearch
+
+" Ignore case when searching
+set ignorecase
+
+" Smart case when searching
+set smartcase
+
+" Enable incremental search
+set incsearch
+
+" Enable undo history
+set undofile
+
+" Use UTF-8 encoding
+set encoding=utf-8
+set fileencoding=utf-8
+
+" Set file format to unix
+set ff=unix
+
+" Auto-indent
+filetype plugin indent on
+
+" NERDTree settings
+autocmd vimenter * if !argc() | NERDTree | endif
+map <C-n> :NERDTreeToggle<CR>
+let NERDTreeShowHidden=1
+
+" Auto-completion settings
+let g:deoplete#enable_at_startup = 1
+
+" Snippet settings
+let g:UltiSnipsExpandTrigger = "<tab>"
+let g:UltiSnipsJumpForwardTrigger = "<tab>"
+let g:UltiSnipsJumpBackwardTrigger = "<s-tab>"
+
+" Automatic tags generation settings
+let g:gutentags_project_root = ['.git', '.svn', '.hg', '.project', 'Makefile']
+let g:gutentags_cache_dir = '~/.cache/tags/'
+let g:gutentags_ctags_executable = '/usr/local/bin/ctags'
+let g:gutentags_ctags_extra_args = ['--tag-relative', '--fields=+n', '--sort=foldcase']
+
+" NERDCommenter settings
+let g:NERDSpaceDelims = 1
+let g:NERDDefaultAlign = 'left'
+let g:NERDCustomDelimiters = {
+  \ 'c': { 'left': '/**', 'right': '*/' }
+  \}
+
+" ConqueShell settings
+autocmd VimEnter * ConqueTermSplit bash
+map <C-t> :ConqueTermSplit bash<CR>
+
 EOF
 
-# Install Dependencies for YouCompleteMe and coc.nvim
-log "Installing dependencies for YouCompleteMe and coc.nvim..."
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  $INSTALL_CMD cmake 2>> "$LOG_FILE"
-else
-  $INSTALL_CMD build-essential cmake 2>> "$LOG_FILE"
+# Install dependencies
+echo "Installing dependencies..."
+nvim +PlugInstall --sync telescope.nvim fzf-lua
+
+# Install CopilotChat.nvim
+echo "Installing CopilotChat.nvim..."
+curl -fLo ~/.local/share/nvim/site/pack/packer/start/CopilotChat.nvim https://github.com/wellle/CopilotChat.nvim/archive/refs/heads/main.zip && unzip -qq ~/.local/share/nvim/site/pack/packer/start/CopilotChat.nvim
+
+# Restart Neovim for changes to take effect
+echo "Restarting Neovim..."
+nvim --headless -c quitall
+
+echo "CopilotChat.nvim and its dependencies are installed!"
+
+# (Optional) Open nvim configuration file
+echo "Do you want to open your nvim configuration file to configure CopilotChat? (y/n)"
+read -r response
+
+if [[ "$response" =~ ^([Yy]) ]]; then
+  echo "Opening nvim configuration..."
+  nvim ~/.config/nvim/init.vim
 fi
 
-# Check for installation errors
-if [ $? -ne 0 ]; then
-  log "Error during dependency installation. Please check $LOG_FILE for details."
-  exit 1
-fi
 
-# Install Plugins
-log "Installing Neovim plugins..."
-nvim +PlugInstall +qall 2>> "$LOG_FILE"
-
-# Check for plugin installation errors
-if [ $? -ne 0 ]; then
-  log "Error during plugin installation. Please check $LOG_FILE for details."
-  exit 1
-fi
-
-# Additional configurations or language support can be added here
-
-# End logging
-log "Neovim and plugins are installed and configured. See $LOG_FILE for details."
+echo "Neovim setup complete!"
 
